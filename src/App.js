ee701e60670da790a3ef05c979aaaef0385fce70
Kat Keff <katkeffner@gmail.com>
2022-11-19 14:23:17 -0500

    removed extras from boilerplate and added Mapview component' ' '

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import Streetview from 'react-google-streetview';
+import Mapview from './components/Mapview'
 
-function App() {
+function StreetView() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Streetview />
     </div>
   );
 }
 
-export default App;
+export default Mapview;
---
+++ src/components/Mapview.js
@@ -0,0 +1,9 @@
+function Mapview() {
+  return (
+    <div className="App">
+      map!
+    </div>
+  );
+}
+
+export default Mapview;
