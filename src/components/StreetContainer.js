15bf04243d2dfb4b80d010d0e52122ab578baaa8
Kat Keff <katkeffner@gmail.com>
2022-11-21 14:01:18 -0500

    both maps successfully rendering on refresh

--- src/components/StreetContainer.js
+++ src/components/StreetContainer.js
@@ -1,20 +1,22 @@
 import React, { Component } from "react";
 import ReactStreetview from "react-streetview";
+import { GoogleMap, LoadScript } from '@react-google-maps/api'
 import APIKey from '../googleAPIKey'
 
-class Street extends React.Component {
-  render() {
+const StreetContainer = () => { {
 
-    const streetViewPanoramaOptions = {
-      position: { lat: 27.289821, lng: -82.481325 },
-      pov: { heading: 100, pitch: 0 },
-      zoom: 1,
-      addressControl: false,
-      showRoadLabels: false,
-      zoomControl: false
-    };
+  const streetViewPanoramaOptions = {
+    position: { lat: 27.289821, lng: -82.481325 },
+    pov: { heading: 100, pitch: 0 },
+    zoom: 1,
+    addressControl: false,
+    showRoadLabels: false,
+    zoomControl: false
+  };
 
     return (
+      <LoadScript
+      googleMapsApiKey={APIKey}>
       <div
         style={{
           width: "50%",
@@ -26,8 +28,9 @@ class Street extends React.Component {
           streetViewPanoramaOptions={streetViewPanoramaOptions}
         />
       </div>
+      </LoadScript>
     );
   }
 }
 
-export default Street;
+export default StreetContainer;
